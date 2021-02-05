3e2e782e33cda09d26d37cda49d08d65be46f75a
GangSugar <74159930+GangSugar@users.noreply.github.com>
2021-02-05 10:18:37 +0800

    Add files via upload

---
+++ 20201011/Test1.java
@@ -0,0 +1,24 @@
+
+public class Test1{
+	public static void main(String[] args) {
+		System.out.println("早上好");
+		System.out.println(Integer.MAX_VALUE);//整型最大值
+		System.out.println(Integer.MIN_VALUE);//最小值
+		double num=1.1;
+		float num1=1.12f;
+		System.out.println(num+","+num1);
+        System.out.println(num*num);
+        System.out.println(num1*num1);//用有限空间表示可能无限的小数
+        char ch='b',ch1='哈';
+        System.out.println(ch1);
+        byte value=0;
+        System.out.println(value);
+        boolean v1=true;
+        System.out.println(v1);
+        String name="zhangsnag";
+        String na1="\"张三\"";//转义字符的使用
+        System.out.println(name);
+        System.out.println(na1);
+	}
+}
+
